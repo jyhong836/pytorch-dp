db625753ea01a40219157d3fa6fd1948aa424623
Junyuan Hong <jyhong836@gmail.com>
2020-05-06 22:16:56 -0400

    Stripe.

--- torchdp/test/privacy_metric_test.py
+++ torchdp/test/privacy_metric_test.py
@@ -4,41 +4,40 @@ from .. import privacy_analysis as tf_privacy
 
 
 class test_CDP(unittest.TestCase):
-    def test_dp_zcdp(self):
+    def setUp(self) -> None:
         # config: uniform schedule
-        sigma = 8.
-        T = 100
-        sample_rate = 1.
-        target_delta = 1e-8
-        alphas = [1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64))
+        self.sigma = 8.
+        self.T = 100
+        self.sample_rate = 1.
+        self.target_delta = 1e-8
+        self.alphas = [1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64))
 
-        zcdp = privacy_metric.zCDP.from_sigma(sigma=sigma)
-        zcdp = privacy_metric.zCDP(rho=zcdp.rho * T)
+    def test_dp_zcdp(self):
+        # zCDP from noise
+        zcdp = privacy_metric.zCDP.from_sigma(sigma=self.sigma)
+        zcdp = privacy_metric.zCDP(rho=zcdp.rho * self.T)
 
-        rdps = tf_privacy.compute_rdp(sample_rate, sigma, 1, alphas)
-        rdp = rdps * T
-        eps, _ = tf_privacy.get_privacy_spent(alphas, rdp, target_delta)
-        dp = privacy_metric.DP(eps, target_delta)
+        # MA DP from noise
+        rdps = tf_privacy.compute_rdp(self.sample_rate, self.sigma, 1, self.alphas)
+        rdp = rdps * self.T
+        eps, _ = tf_privacy.get_privacy_spent(self.alphas, rdp, self.target_delta)
+        dp = privacy_metric.DP(eps, self.target_delta)
 
-        print(zcdp.to_dp(target_delta).eps, dp.eps)
-        self.assertAlmostEqual(zcdp.to_dp(target_delta).eps, dp.eps, places=3)
+        self.assertAlmostEqual(zcdp.to_dp(self.target_delta).eps, dp.eps, places=3)
+
+        # DP to zCDP
+        zcdp = privacy_metric.zCDP.from_dp(dp)
+        self.assertAlmostEqual(zcdp.to_dp(self.target_delta).eps, dp.eps, places=3)
 
     def test_dp_ctcdp(self):
-        # config: uniform schedule
-        sigma = 8.
-        T = 100
-        sample_rate = 1.
-        target_delta = 1e-8
-        alphas = [1 + x / 10.0 for x in range(1, 100)] + list(range(12, 64))
-
-        rdps = tf_privacy.compute_rdp(sample_rate, sigma, 1, alphas)
-        rdp = rdps * T
-        eps, _ = tf_privacy.get_privacy_spent(alphas, rdp, target_delta)
-        dp = privacy_metric.DP(eps, target_delta)
+        # MA DP from noise
+        rdps = tf_privacy.compute_rdp(self.sample_rate, self.sigma, 1, self.alphas)
+        rdp = rdps * self.T
+        eps, _ = tf_privacy.get_privacy_spent(self.alphas, rdp, self.target_delta)
+        dp = privacy_metric.DP(eps, self.target_delta)
 
         # only test forward and backward
         ctcdp = privacy_metric.ctCDP.from_dp(dp)
         ctcdp = privacy_metric.ctCDP(rho=ctcdp.rho)
 
-        print(ctcdp.to_dp(target_delta).eps, dp.eps)
-        self.assertAlmostEqual(ctcdp.to_dp(target_delta).eps, dp.eps, places=3)
+        self.assertAlmostEqual(ctcdp.to_dp(self.target_delta).eps, dp.eps, places=3)
