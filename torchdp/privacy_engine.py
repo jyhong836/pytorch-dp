FIX: the privacy budget should only be requested once for each gradient query.

torchdp/privacy_engine.py
@@ -55,6 +55,7 @@ class PrivacyEngine:
         self.privacy_budget = privacy_budget
         if privacy_budget is not None:
             self._residual_budget = privacy_budget
+            # self._accumulate_cost = privacy_budget.zero()
 
         self.secure_seed = int.from_bytes(os.urandom(8), byteorder="big", signed=True)
         self.secure_generator = (
@@ -143,15 +144,19 @@ class PrivacyEngine:
                 raise DPOutOfBudgetError(f"Request budget ({cost:.3g}) > "
                                          f"residual ({self._residual_budget:.3g}) at step {self.steps}.")
             else:
+                # self._accumulate_cost = self._accumulate_cost.compose([self._accumulate_cost, cost])
+                # FIXME The substraction may only be defined for some metric but compose is for all qualified metrics.
                 self._residual_budget = self._residual_budget - cost
+                # diff = self.privacy_budget.compose([self._accumulate_cost, self._residual_budget]) - self.privacy_budget
+                # assert(abs(diff.rho) < 1e-15)
         return self.noise_multiplier
 
     def step(self):
         self.steps += 1
         max_norm = self.clipper.step()
+        noise_multiplier = self.request_budget()
         for p in self.module.parameters():
             if p.requires_grad and self.noise_multiplier > 0:
-                noise_multiplier = self.request_budget()
                 noise = torch.normal(
                     0,
                     noise_multiplier * max_norm,

torchdp/privacy_metric/CDP.py
@@ -391,9 +391,11 @@ class ctCDP(tCDP):
         # TODO the assertion may fail. We only constraint the rho to satisfy the condition for simplicity.
         #     assert self.rev_omega * np.log(1 / sample_rate) / 4 >= self.rho , "Invalid sample rate for the fixed " \
         #                                                                      "rev_omega."
-        return ctCDP(rho=13. * self.rho * sample_rate ** 2)
-        # return ctCDP(rho=self.rho * sample_rate ** 2)
+        # These should be checked using MA.
+        # return ctCDP(rho=13. * self.rho * sample_rate ** 2)
+        return ctCDP(rho=self.rho * sample_rate ** 2)
 
     def deamp_by_sampling(self, sample_rate: float, batch_type="random") -> PrivacyMetric:
         assert batch_type == "random"
         return ctCDP(rho=self.rho / (sample_rate ** 2))  # (13. * sample_rate ** 2))
+        # return ctCDP(rho=self.rho / (13. * sample_rate ** 2))